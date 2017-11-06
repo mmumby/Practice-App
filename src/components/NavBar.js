186deee62cfbba3492e7fa6e0835d27be145c5f6
mmumby <meredith.mumby@outlook.com>
2017-11-05 20:25:17 -0700

    added menu for links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/App.css';
 
 import Routes from '../routes';
 
-export default () => (
-  <div className="navbar">
-    <Routes />
-  </div>
-  );
+
+class NavBar extends Component {
+  constructor(){
+    super()
+    this.state ={
+      isOpen: false
+    };
+  }
+
+  handleButtonClick(){
+    this.setState({
+      isOpen: true
+    });
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <i onClick={this.handleButtonClick.bind(this)} id="hamburger-nav" class="fa fa-3x fa-bars" aria-hidden="true"></i>
+        <Routes />
+      </div>
+    );
+  }
+}
+
+export default NavBar;
