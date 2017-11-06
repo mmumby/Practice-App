362d92f0dd97fca754143376fe95dd4e50efac78
mmumby <meredith.mumby@outlook.com>
2017-11-05 18:05:00 -0700

    added example 1 flex divs

--- src/components/FlexExamples.js
+++ src/components/FlexExamples.js
@@ -5,6 +5,14 @@ class FlexExample extends Component {
   render() {
     return (
       <div className="examples-container">
+        <div className="container-one">
+          <div className="one-example-one">
+          </div>
+          <div className="one-example-two">
+          </div>
+          <div className="one-example-three">
+          </div>
+        </div>
       </div>
     );
   }
