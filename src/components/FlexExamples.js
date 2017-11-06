fd83c0bf5eb612a67cc26b9a6c11022ee2e43eb3
mmumby <meredith.mumby@outlook.com>
2017-11-05 18:16:37 -0700

    added some more flex exampled to flex-container

--- src/components/FlexExamples.js
+++ src/components/FlexExamples.js
@@ -7,12 +7,40 @@ class FlexExample extends Component {
       <div className="examples-container">
         <div className="container-one">
           <div className="one-example-one">
+            <p> 1 </p>
           </div>
           <div className="one-example-two">
+            <p> 2 </p>
           </div>
           <div className="one-example-three">
+            <p> 3 </p>
           </div>
         </div>
+
+        <div className="container-two">
+          <div className="two-example-one">
+            <p> 1 </p>
+          </div>
+          <div className="two-example-two">
+            <p> 2 </p>
+          </div>
+          <div className="two-example-three">
+            <p> 3 </p>
+          </div>
+        </div>
+
+        <div className="container-three">
+          <div className="three-example-one">
+            <p> 1 </p>
+          </div>
+          <div className="three-example-two">
+            <p> 2 </p>
+          </div>
+          <div className="three-example-three">
+            <p> 3 </p>
+          </div>
+        </div>
+
       </div>
     );
   }
